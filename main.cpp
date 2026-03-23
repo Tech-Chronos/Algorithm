ae720b7106e3cd0e9e321e70bd9478920fe6cf90
dsj <dongsijie8@gmail.com>
2026-03-23 22:39:26 +0800

    demo

--- main.cpp
+++ main.cpp
@@ -3,33 +3,72 @@
 
 using namespace std;
 
+//class Solution
+//{
+//public:
+//    static int lengthOfLongestSubstring(string s)
+//    {
+//        int hash[512] = { 0 };
+//        int ret = 0;
+//        for (int left = 0, right = 0; right < s.size(); ++right)
+//        {
+//            hash[right]++;
+//
+//            while (hash[right] == 2)
+//            {
+//                hash[left]--;
+//                ++left;
+//            }
+//
+//            ret = max(ret, right - left  + 1);
+//        }
+//        return ret;
+//    }
+//};
+
 class Solution
 {
 public:
-    static int lengthOfLongestSubstring(string s)
+    static string addStrings(string& num1, string& num2)
     {
-        int hash[512] = { 0 };
-        int ret = 0;
-        for (int left = 0, right = 0; right < s.size(); ++right)
+        int size1 = num1.size() - 1;
+        int size2 = num2.size() - 1;
+        string ret;
+        char add = 0;
+        char other = 0;
+
+        int size = max(size1, size2);
+
+        for (int i = size1, j = size2; size > 0; --size)
         {
-            hash[right]++;
+            char left, right;
+            if (i <= 0) left = 0;
+            if (j <= 0) right = 0;
+            if (i > 0)  left = num1[i];
+            if (j > 0)  right = num2[j];
 
-            while (hash[right] == 2)
+            add = (int)left + (int)right + (int)other;
+            if (add >= 10)
             {
-                hash[left]--;
-                ++left;
+                add -= 10;
+                other = 1;
             }
+            ret += to_string((int)add);
 
-            ret = max(ret, right - left  + 1);
+            --i;
+            --j;
         }
+        if (other != 0) ret += other;
+
         return ret;
     }
 };
 
 int main()
 {
-    string str = "abcabcbb" ;
-    int ret = Solution::lengthOfLongestSubstring(str);
+    string num1 = "456", num2 = "77";
+
+    Solution::addStrings(num1, num2);
 
     return 0;
 }
